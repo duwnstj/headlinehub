01532a4727bacda0852cae6fc06f2d8af3659718
ican0422 <ican0422@gmail.com>
2024-09-02 22:49:30 +0900

    [update] : 회원가입 기능 구현

--- src/main/java/com/sparta/headlinehub/controller/UserController.java
+++ src/main/java/com/sparta/headlinehub/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.sparta.headlinehub.controller;
 
+import com.sparta.headlinehub.dto.user.request.PostUserSaveRequestDto;
+import com.sparta.headlinehub.dto.user.response.PostUserSaveResponseDto;
+import com.sparta.headlinehub.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService service;
+
+    /**
+     * 회원 가입
+     * @param requestDto 유저 이메일, 유저 비밀번호, 유저 이름, 유저 전화번호
+     * @return 가입한 유저 이름
+     */
+    @PostMapping()
+    public PostUserSaveResponseDto saveUser(@RequestBody PostUserSaveRequestDto requestDto) {
+        return service.saveUser(requestDto);
+    }
+
+    // 회원 로그인
+
+    // 회원 탈퇴
 }
