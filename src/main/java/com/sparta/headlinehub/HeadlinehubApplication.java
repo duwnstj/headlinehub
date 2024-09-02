1a85873c7a7a791a2950fd4ef5d6c032d14d89a3
ican0422 <ican0422@gmail.com>
2024-09-02 11:31:03 +0900

    [add] : git init

---
+++ src/main/java/com/sparta/headlinehub/HeadlinehubApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.headlinehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlinehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlinehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/headlinehub/HeadlinehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.headlinehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlinehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
