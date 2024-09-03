5e07e186d53f77e65180ebe7560995cf11aebc89
여준서 <duwnstj12@naver.com>
2024-09-03 18:30:46 +0900

    [add] : Exception 커스텀 클래스 생성

---
+++ src/main/java/com/sparta/headlinehub/exception/InvalidPrivilegeException.java
@@ -0,0 +1,9 @@
+package com.sparta.headlinehub.exception;
+
+
+public class InvalidPrivilegeException extends RuntimeException {
+
+    public InvalidPrivilegeException(String messeage){
+        super(messeage);
+    }
+}
