155195df8e9ef7926c44745063d4b6cfbab243d8
여준서 <duwnstj12@naver.com>
2024-09-03 18:31:07 +0900

    [add] : UserDto 클래스 생성

---
+++ src/main/java/com/sparta/headlinehub/dto/user/UserDto.java
@@ -0,0 +1,11 @@
+package com.sparta.headlinehub.dto.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDto {
+
+    private final String userName;
+}
